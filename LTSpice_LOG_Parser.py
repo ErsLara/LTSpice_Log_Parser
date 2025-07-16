89b7acf8507d9c40ad906e1dee87c60abe1e58c3
Infante Lara <uidm8570@contiwan.com>
2025-07-16 10:52:34 +0200

    Seacht for multiple LTSpice .log files in script folder and generate respective overview files

--- LTSpice_LOG_Parser.py
+++ LTSpice_LOG_Parser.py
@@ -1,35 +1,37 @@
 import re
 import pandas as pd
+import os
 
-log_file_name = r".\LTSpice_Example\LTSPICE_Example.log"
+log_files = [f for f in os.listdir('.') if f.endswith('.log')]
 
-LTSpiceLOG = open(log_file_name, "rt")
-
-log_content = LTSpiceLOG.read()
-
-LTSpiceLOG.close()
-
-pattern = re.compile(r"^Measurement:\s(.+)|^\s+\d+\s(-?\d*\.*\d*e?-?\d+)", re.MULTILINE)
-
-meas_results = pattern.findall(log_content)
-
-meas_dict = {}
-
-for signal, value in meas_results:
-    if signal:
-        signal_name = signal
-        meas_dict[signal_name] = []
-    elif value:
-        meas_dict[signal_name].append(float(value))
-
-meas_results_df = pd.DataFrame(meas_dict)
-
-pd.set_option('display.max_columns', None)
-overview = meas_results_df.describe(percentiles=[], include='all')
-overview = overview.drop("50%")
-
-output_file_name = log_file_name.replace(".log", "_meas_overview.txt")
-
-output_file = open(output_file_name, "w")
-output_file.write(str(overview))
-output_file.close()
+for log_file_name in log_files:
+    LTSpiceLOG = open(log_file_name, "rt")
+    
+    log_content = LTSpiceLOG.read()
+    
+    LTSpiceLOG.close()
+    
+    pattern = re.compile(r"^Measurement:\s(.+)|^\s+\d+\s(-?\d*\.*\d*e?-?\d+)", re.MULTILINE)
+    
+    meas_results = pattern.findall(log_content)
+    
+    meas_dict = {}
+    
+    for signal, value in meas_results:
+        if signal:
+            signal_name = signal
+            meas_dict[signal_name] = []
+        elif value:
+            meas_dict[signal_name].append(float(value))
+    
+    meas_results_df = pd.DataFrame(meas_dict)
+    
+    pd.set_option('display.max_columns', None)
+    overview = meas_results_df.describe(percentiles=[], include='all')
+    overview = overview.drop("50%")
+    
+    output_file_name = log_file_name.replace(".log", "_meas_overview.txt")
+    
+    output_file = open(output_file_name, "w")
+    output_file.write(str(overview))
+    output_file.close()
